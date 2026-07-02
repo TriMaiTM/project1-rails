cfff3ba993d363ed8de8506ed7b994bc97d2671d
TriMaiTM <tridv.22it@vku.udn.vn>
2026-07-02 16:49:07 +0700

    fix: lint and test

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "static_pages/about"
     resources :microposts
     resources :users
-  root 'users#index'
+  root "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
